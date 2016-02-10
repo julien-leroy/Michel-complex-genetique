657820076925d0cd6bd044891f7f55ce96aec663
julien leroym <julien@leroy.com>
2016-02-10 15:06:17 +0100

    tournament methode work

--- main.py
+++ main.py
@@ -5,19 +5,19 @@ from randm import randm
 tabGen = []
 
 def main():
-	for x in xrange(0,10):
-		nextGen()
-		print tabGen[x].tournament()
-		raw_input()
+	# for x in xrange(0,10):
+	# 	nextGen()
+	# 	print tabGen[x].tournament()
+	# 	raw_input()
 	
-	#print gen 
+	gen = generation()
 	#print gen.printPedro()
-	fin1 = gen.RWS_()
-	print gen.RWS_()
+	fin1 = gen.tournament()
+	print fin1()
 
 	# print gen.getAllFitness()
-	fin2 = gen.copulation(fin1)
-	print fin2
+	# fin2 = gen.copulation(fin1)
+	# print fin2
 
 def nextGen():
 	print("gen numero : " + str(len(tabGen) + 1))
