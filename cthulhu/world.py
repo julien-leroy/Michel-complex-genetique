ca4b51062fee7a172e36bc93b627de624775cd0f
walrand degeorges <w.degeorges@gmail.com>
2016-03-23 20:20:06 +0100

    RWS + test world OK

--- cthulhu/world.py
+++ cthulhu/world.py
@@ -3,6 +3,9 @@ from generation import *
 
 class World:
 
+    azeret = raw_input('Number of generations ?  ')
+    nbGenerations =int(azeret) #doit etre pair
+
     genInit = Generation()
     print genInit.tabIndividus
 
@@ -10,17 +13,19 @@ class World:
         self.initWorld()
 
     def initWorld(self):
-        i=0
-        while i < len(self.genInit.tabIndividus)/2:
-            tab = []
-            #tab.append(self.genInit.copulation(self.genInit.tabIndividus[0],self.genInit.tabIndividus[1]))
-            etape1 = self.genInit.RWS()
-            print "etape1: " + str(etape1)
-            #print "etape1" + str(etape1)
-            etape2 = self.genInit.copulation(etape1[0],etape1[1])
-            #tab.append(etape2[0])
-            #tab.append(etape2[1])	
-            tab.append(etape2[0])
-            tab.append(etape2[1])
-            print "TAB TAB TAB" + str(tab)
-            i+=1
+        for y in range(0,self.nbGenerations):
+            print "GENRATION NUM." + str(y)
+            i=0
+            while i < len(self.genInit.tabIndividus)/2:
+                tab = []
+                #tab.append(self.genInit.copulation(self.genInit.tabIndividus[0],self.genInit.tabIndividus[1]))
+                etape1 = self.genInit.RWS()
+                #print "etape1: " + str(etape1)
+                #print "etape1" + str(etape1)
+                etape2 = self.genInit.copulation(etape1[0],etape1[1])
+                #tab.append(etape2[0])
+                #tab.append(etape2[1])	
+                tab.append(etape2[0])
+                tab.append(etape2[1])
+                print "TAB TAB TAB" + str(tab)
+                i+=1
