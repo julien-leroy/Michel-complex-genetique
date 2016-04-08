766c0382ada0e26de7213d823d1c625e33b4ef9e
walrand degeorges <w.degeorges@gmail.com>
2016-04-08 11:00:33 +0200

    Initialisation du projet F.O.U.R.M.I.

--- fourmi/fourmi.py
+++ fourmi/fourmi.py
@@ -1,31 +1,13 @@
 class fourmi:
 	def __init__(self):
-		self.tabGenes = []
-		self.score    = 0
-		self.initGenes()
-		self.fitness()
+		self.tabVillesPassees = []
+		self.initFourmi()
 
-	def initGenes(self):
-		self.tabGenes = self.rnmdz()
+	def initFourmi(self):
+		#remplir avec les caracteristiques d une fourmi
 		return self
 
-	def fitness(self):
-		self.score = sum(self.tabGenes)
+
+	def layPheromon(self):
+		#remplir avec le code pour deposer une pheromone sur la zone
 		return self
-
-	def setGenes(self, newGen):
-		self.tabGenes = newGen
-		self.fitness()
-
-	def rnmdz(self):
-		liste = []
-		for walrandDegorge in range(0,self.nbGenesInt):
-			liste.append(randint(0,10))
-		# print liste
-		return liste
-
-	def getGenes(self):
-		return self.tabGenes
-
-	def getFitness(self):
-		return self.score
