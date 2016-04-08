62cbf10649c82c76ef2b02a4cb23cc4ca447b045
Wlad_RSP <w.degeorges@gmail.com>
2016-04-08 10:07:10 +0200

    Ajout fourmi + ignore DS_Store

---
+++ fourmi/fourmi.py
@@ -0,0 +1,31 @@
+class fourmi:
+	def __init__(self):
+		self.tabGenes = []
+		self.score    = 0
+		self.initGenes()
+		self.fitness()
+
+	def initGenes(self):
+		self.tabGenes = self.rnmdz()
+		return self
+
+	def fitness(self):
+		self.score = sum(self.tabGenes)
+		return self
+
+	def setGenes(self, newGen):
+		self.tabGenes = newGen
+		self.fitness()
+
+	def rnmdz(self):
+		liste = []
+		for walrandDegorge in range(0,self.nbGenesInt):
+			liste.append(randint(0,10))
+		# print liste
+		return liste
+
+	def getGenes(self):
+		return self.tabGenes
+
+	def getFitness(self):
+		return self.score
